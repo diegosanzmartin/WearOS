94875a9d7a809e1732c658880befc91f352fc859
Diego Sanz Martín <diego.sm.acc@gmail.com>
2025-06-01 17:23:18 +0200

    feat: add clickable modifier to DailySleepCarousel for improved interactivity

--- Wfit-sleep/app/src/main/java/com/example/wfit/presentation/components/DailySleepCarousel.kt
+++ Wfit-sleep/app/src/main/java/com/example/wfit/presentation/components/DailySleepCarousel.kt
@@ -2,6 +2,7 @@ package com.example.wfit.presentation.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
